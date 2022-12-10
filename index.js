d9479535e31444663cd3d3edb53c64cc9aaa5f48
ChelseaSaunders <c_chilcoat@msn.com>
2022-12-09 16:37:31 -0800

    oops minor error corrected

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (_req, res) => {
-  res.send('<h1><Oh hi, World!</h1>');
+  res.send("<h1>Oh hi, world!</h1>");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
